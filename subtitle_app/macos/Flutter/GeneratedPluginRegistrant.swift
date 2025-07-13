88025a7e72c0095c8f3101ae9e7ea29966722846
mehmet ünlü <152738918+unlumehmet30@users.noreply.github.com>
2025-07-13 22:56:50 +0300

    apk release

--- subtitle_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ subtitle_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import file_picker
 import package_info_plus
 import path_provider_foundation
 import video_player_avfoundation
 import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
